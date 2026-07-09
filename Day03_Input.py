890d3b7d8d76398c15173349483aa75ffde8e1b2
P <rajeshpalakur@gmail.com>
2026-07-09 08:10:33 +0530

    Day 3 -Input function

---
+++ Day03_Input.py
@@ -0,0 +1,18 @@
+#What is input()?
+
+# input() allows python to receive the data from user.
+
+
+# Day3 Input function
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print("Name : ", name)
+print("Age: ", age)
+print("city: ",city)
+
+print(type(name))
+print(type(age))
+print(type(city))
