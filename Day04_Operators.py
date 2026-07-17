6b77b7ab0d6020662d97d2c0b8d2b905d411c668
P <rajeshpalakur@gmail.com>
2026-07-17 10:18:43 +0530

    Day 4 Operators and Day 5 If Statement

---
+++ Day04_Operators.py
@@ -0,0 +1,21 @@
+# Day 04- Operators
+
+a=10
+b=20
+age = 26
+
+print("Addition: ", a+b)
+print("Subtraction:" , a-b)
+print("Multiplication: ", a*b)
+print("Division:", a/b)
+
+print("Equal: ", a==b)
+print("Not Equal: ", a!=b)
+print("Greaterthan: ", b>a)
+print("Lessthan: ", a<b)
+print("Modulas: " , b%a)
+print("Modulas: ", a%b)
+
+print(a>35)
+print(b<=20)
+print(age>35)
---
+++ Day05_If_Statement.py
@@ -0,0 +1,39 @@
+# Why Do We Need If?
+#Imagine:
+#age = 26
+#Question:
+#Can this person vote?
+#Python must make a decision.
+#That's where if comes in.
+
+# Excercise1
+
+age =26
+if age>=18:
+    print("Eligible to vote")
+
+# Exercise 2
+
+marks = 80
+if marks >=35:
+    print("pass")
+
+# Exercise 3
+
+salary = 30000
+if(salary>50000):
+    print("Eligible")
+
+# Exercise 4
+
+SSO = 986863
+if SSO != 454738: 
+    print("wrong sso")
+print("Program Ended")
+
+# Exercise 5
+
+number = 10
+if number == 10 :
+    print("Correct")
+print("Program Ended")
