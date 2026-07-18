58838505759f6a8808cf978a8a54e7eac8977df2
P <rajeshpalakur@gmail.com>
2026-07-18 20:13:38 +0530

    Adding Day 09 While loop

---
+++ Day09_While_Loop.py
@@ -0,0 +1,35 @@
+# Day 09 - While loop
+
+# Example 1
+
+count = 1
+
+while count<= 5:
+    print(count)
+    count = count + 1
+
+# Example 2
+
+count = 3
+
+while count<=6:
+    print(count)
+    count=count+2
+
+# Example 3
+
+name = input("Enter your name: ")
+
+count = 1
+while count <= 5:
+    print(name)
+    count = count + 1
+
+# Example 4
+
+count = 2
+
+while count<=5:
+    print(count)
+    count = count +2
+print("End")
