29b18b842e1ceeaed8d7c59efe7c687165590182
P <rajeshpalakur@gmail.com>
2026-07-18 19:39:14 +0530

    Day 08 For loop.Py

---
+++ Day08_For_Loop.py
@@ -0,0 +1,40 @@
+# What Is a Loop?
+
+#A loop executes the same code multiple times.
+
+# Example: 1
+
+for i in range(5):
+    print("Hello ", i)
+
+# Example 2
+
+name = input("Enter your name: ")
+for i in range(8):
+    print(name, i)
+
+# Example 3
+
+for i in range(2,6):
+    print(i)
+
+# Example 4
+
+for i in range(4,10):
+    print("SDET")
+
+# Example 5
+
+for banana in range(5):
+    print(banana)
+
+# Example 5
+
+for i in range(2,6):
+    print("Iteration")
+    print(i)
+
+# Example 6
+
+for i in range(10):
+    print("Executing Testcases")
